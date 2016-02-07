95f5a0b773cb3768d0187939af0d534e670c7ddd
David Vilar <summun@gmail.com>
2016-02-07 20:19:13 +0000

    second version that almost work

--- decrypt/src/decrypt/core.clj
+++ decrypt/src/decrypt/core.clj
@@ -4,36 +4,35 @@
             [buddy.core.codecs :as codecs]
             [clojure.data.codec.base64 :as b64]))
 
-(def algorithm :aes128-cbc-hmac-sha256)
-
 ; (def iv (nonce/random-bytes 16))   ;; 16 bytes random iv
 (def iv (codecs/str->bytes "aaaaaaaaaaaaaaaa"))
 ; (def secret-key (hash/sha256 "mysecret")) ;; 32 bytes key
-(def secret-key (codecs/str->bytes "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+(def secret-key (codecs/str->bytes "aaaaaaaaaaaaaaaa"))
+
+(def cipher (crypto/block-cipher :aes :cbc))
 
 (def filename "../out/encrypted-clojure")
 
 (defn- encrypt [text]
   (println "encrypting" text)
-  (-> (codecs/str->bytes text)
-      (crypto/encrypt secret-key iv {:algorithm algorithm})
-      (b64/encode)
-      (codecs/bytes->str)))
+  (let [input  (codecs/str->bytes text)]
+    (-> (crypto/encrypt-cbc cipher input secret-key iv)
+        (b64/encode)
+        (codecs/bytes->str))))
+
+(defn- encrypt-and-save [text]
+  (->> (encrypt text)
+       (spit filename)))
 
 (defn- print-and-return [text]
   (println "decrypting" text)
   text)
 
 (defn- decrypt [filename]
-  (-> (slurp filename)
-      (print-and-return)
-      (codecs/str->bytes)
-      (b64/decode)
-      (crypto/decrypt secret-key iv {:algorithm algorithm})
-      (codecs/bytes->str)))
-
-(def encrypted-text (slurp "../encrypted"))
+  (let [input (-> (slurp filename) (print-and-return) (codecs/str->bytes) (b64/decode))]
+    (-> (crypto/decrypt-cbc (crypto/block-cipher :aes :cbc) input secret-key iv)
+        (codecs/bytes->str))))
 
 (defn -main [& args]
-  (->> (encrypt "Banana split FTW") (spit filename))
+  (encrypt-and-save "Banana split FTW")
   (println "plain is" (decrypt filename)))
