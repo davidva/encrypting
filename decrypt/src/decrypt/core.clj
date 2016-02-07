6d0fa95ac38c97ea648d47997ca7d836e33df456
David Vilar <summun@gmail.com>
2016-02-07 21:20:06 +0000

    it definitely works

--- decrypt/src/decrypt/core.clj
+++ decrypt/src/decrypt/core.clj
@@ -2,37 +2,28 @@
   (:gen-class)
   (:require [buddy.core.crypto :as crypto]
             [buddy.core.codecs :as codecs]
-            [clojure.data.codec.base64 :as b64]))
+            [clojure.data.codec.base64 :as b64]
+            [buddy.core.bytes :as bytes]))
 
-; (def iv (nonce/random-bytes 16))   ;; 16 bytes random iv
-(def iv (codecs/str->bytes "aaaaaaaaaaaaaaaa"))
-; (def secret-key (hash/sha256 "mysecret")) ;; 32 bytes key
 (def secret-key (codecs/str->bytes "aaaaaaaaaaaaaaaa"))
 
 (def cipher (crypto/block-cipher :aes :cbc))
 
-(def filename "../out/encrypted-clojure")
+(def filename "../out/encrypted")
 
-(defn- encrypt [text]
-  (println "encrypting" text)
-  (let [input  (codecs/str->bytes text)]
-    (-> (crypto/encrypt-cbc cipher input secret-key iv)
-        (b64/encode)
-        (codecs/bytes->str))))
-
-(defn- encrypt-and-save [text]
-  (->> (encrypt text)
-       (spit filename)))
-
-(defn- print-and-return [text]
+(defn- decrypt [text iv]
   (println "decrypting" text)
-  text)
-
-(defn- decrypt [filename]
-  (let [input (-> (slurp filename) (print-and-return) (codecs/str->bytes) (b64/decode))]
+  (let [input (b64/decode (codecs/str->bytes text))
+        iv    (bytes/slice (b64/decode (codecs/str->bytes iv)) 0 16)]
+    (println (count iv))
     (-> (crypto/decrypt-cbc (crypto/block-cipher :aes :cbc) input secret-key iv)
         (codecs/bytes->str))))
 
+(defn- decrypt-from-file [filename]
+  (let [content (clojure.string/split (slurp filename) #"--")
+        text    (first content)
+        iv      (last content)]
+    (decrypt text iv)))
+
 (defn -main [& args]
-  (encrypt-and-save "Banana split FTW")
-  (println "plain is" (decrypt filename)))
+  (println "plain is" (decrypt-from-file filename)))
--- encrypt.rb
+++ encrypt.rb
@@ -4,31 +4,29 @@ require 'base64'
 
 ALG = 'aes-128-cbc'
 KEY = 'aaaaaaaaaaaaaaaa'
-IV = 'aaaaaaaaaaaaaaaa'
 FILENAME = 'out/encrypted'
 
 Dir.mkdir('out') unless File.exists?('out')
-
-# puts OpenSSL::Cipher.ciphers.select { |c| c.downcase.include?('aes') }
 
 def encrypt(text)
   puts "encrypting #{text}"
   cipher = OpenSSL::Cipher::Cipher.new(ALG)
   cipher.encrypt
   cipher.key = KEY
-  cipher.iv = IV
+  cipher.iv = iv = cipher.random_iv
+  puts iv.size
   encrypted = cipher.update(text)
   encrypted << cipher.final
-  encrypted64 = Base64.encode64(encrypted)
-  File.open(FILENAME, 'w') { |file| file.puts(encrypted64) }
+  encrypted64 = Base64.encode64(encrypted).strip
+  File.open(FILENAME, 'w') { |file| file.puts("#{encrypted64}--#{Base64.encode64(iv)}".strip) }
 end
 
 def decrypt(filename = FILENAME)
+  (encrypted64, iv) = File.readlines(FILENAME).first.split('--')
   decipher = OpenSSL::Cipher::Cipher.new(ALG)
   decipher.decrypt
   decipher.key = KEY
-  decipher.iv = IV
-  encrypted64 = File.readlines(FILENAME).first
+  decipher.iv = Base64.decode64(iv)
   puts "decrypting #{encrypted64}"
   encrypted = Base64.decode64(encrypted64)
   plain = decipher.update(encrypted)
